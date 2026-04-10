6b8a9922bf70ecd06ac16aa761ee95d15f8aee90
Prarthanan A D <94905649+Prarthana5@users.noreply.github.com>
2026-04-10 17:24:06 +0000

    dataTypes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,40 @@
+"use strict" // treat all code as newer version
+
+//alert(3+3) // we are using nodejs not brower that why it will give error
+
+let name="Prarthana"
+let age = 13
+let islLog = true
+
+// primitive
+//number
+//bigint
+//string- single code and single quote
+//boolean -true/false
+//null - standalone value - empty
+//undefined - variable declared value not defined 
+// symbol - to find unique
+
+
+// non primitive
+
+//object
+console.log(typeof "hi");
+console.log(typeof age);
+console.log(typeof undefined); // type-undefined
+console.log(typeof null); // type - object
+
+// null is an object
+
+
+
+
+
+
+
+
+
+
+
+
+
